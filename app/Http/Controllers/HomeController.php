f5a4439a5f99b93556f1a54c78de5536833dc452
Sagesse <dihambouroslyn@gmail.com>
2020-01-29 00:48:47 +0000

    tout passe

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -54,57 +54,11 @@ class HomeController extends Controller
                 Session::put('user_role', $data->user_role);
                 Session::put('admin_image', $data->admin_image);
                 $redirect ='/dashboard';
+		$errors =  Session::put('message', null);
+			
             }
         }
         return redirect($redirect)->with($errors);
     }
 
-//    public function login(Request $request)
-//    {
-//        request()-> validate([
-//            'admin_email' => ['required', 'email'],
-//            'admin_password' => ['required', 'min:8']
-//        ]);
-//        $admin_email = $request->admin_email;
-//        $admin_password = md5($request->admin_password);
-//        $result2 = DB::table('tbl_admin')
-//            ->where('admin_email',$admin_email)
-//            ->where('admin_password', $admin_password)
-//            ->where('admin_status', 0)
-//            ->first();
-//        $result = DB::table('tbl_admin')
-//            ->where('admin_email',$admin_email)
-//            ->where('admin_password', $admin_password)
-//            ->where('admin_status', 1)
-//            ->first();
-//        if($result){
-//                Session::put('admin_structure', $result->admin_structure);
-//                Session::put('admin_prenom', $result->admin_prenom);
-//                Session::put('admin_phone', $result->admin_phone);
-//                Session::put('admin_role', $result->admin_role);
-//                Session::put('admin_id', $result->admin_id);
-//                Session::put('admin_email', $result->admin_email);
-//                Session::put('admin_phone', $result->admin_phone);
-//                Session::put('user_role', $result->user_role);
-//                Session::put('admin_image', $result->admin_image);
-//               return redirect::to('/dashboard');
-//        }
-//
-//        if($result2){
-//            return Redirect::to('/admin')
-//                ->withInput()->withErrors([
-//                    'admin_email' => 'Veuillez activer votre Compte avant de vous connectez',
-//                ]);
-//
-//        }
-//        else{
-//            return Redirect::to('/admin')
-//                ->withInput()->withErrors([
-//                    'admin_email' => 'Vos  identifiants sont incorrects',
-//
-//                ]);
-//
-//        }
-//
-//    }
 }
